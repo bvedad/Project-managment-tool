ae77608c58b8cee0d6b23f44738b418539a06113
Vedad Burgić <vedad.burgic@atlantbh.com>
2018-05-19 09:06:48 +0200

    Navigations for user and admin implemented in index.html file. Javascript for Login route moved to separate file; Routes for user implemented: 	1. User main route; 	2. User homework list; Routes for admin implemented; 	1. Admin main route; 	2. Admin homework list; 	3. Admin post homework.

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
-    
-    $("#my-button").click(function() {
-        console.log('**********')
+    $("#user-navigation").hide();
+    $("#admin-navigation").hide();
+    $("#login-button").click(function() {
+        console.log(document.getElementById('username-input').value);
+        // document.getElementById('login-button').click();
     });
 });
---
+++ assets/js/user/user.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $("#user-navigation").show();
+    $("#admin-navigation").hide();
+    location.href = location.href + "-homework-list"
+});
